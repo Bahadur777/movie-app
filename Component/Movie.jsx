eb9a747857b2decc314a26ec6117e8af633354b5
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-01-09 21:52:45 +0600

    fetch problem solve

--- Component/Movie.jsx
+++ Component/Movie.jsx
@@ -1,25 +1,55 @@
-import React from 'react'
+"use client";
 
-const Movie =async() => {
-    const url = 'https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '3ac5132cb1msh052a673f7ba3879p1b0f21jsn559234a95b4b',
-		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-	}
+import React, { useState, useEffect } from "react";
+
+const Movie = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url =
+        "https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr";
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "3ac5132cb1msh052a673f7ba3879p1b0f21jsn559234a95b4b",
+          "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log("this is the result", result);
+
+        if (result.d && result.d.length > 0) {
+          setMovies(result.d);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div>Movie</div>
+      <ul className="">
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            {movie.i && (
+              <>
+                <img src={movie.i.imageUrl} alt={movie.l} />
+                <p>{movie.l}</p>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log("this is resutl", result);
-} catch (error) {
-	console.error(error);
-}
-  return (
-    <div>Movie</div>
-  )
-}
-
-export default Movie
+export default Movie;
